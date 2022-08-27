60b26ce518240b16d9458a768bf92f073cb5672f
Tim <truecarry@gmail.com>
2022-08-27 16:11:26 +0300

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until $(curl --output /dev/null --silent --head --fail http://localhost:8082/api/v2/getMasterchainInfo); do
+until $(curl --output /dev/null --silent --fail http://localhost:8082/api/v2/getMasterchainInfo); do
     printf '.'
     sleep 5
 done
